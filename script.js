a42eaa0a7ec7492be7530bc12e9b3cde195c89b6
colta-cristi <20718625+colta-cristi@users.noreply.github.com>
2019-09-17 17:01:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let team = document.querySelector('#team-a');
+
+
+team.addEventListener('click', function(e) {
+	if (e.target.tagName !== 'IMG' && !e.target.classList.contains('overlay')) return;
+	let target = e.target;
+
+	if (target.classList.contains('overlay')) {
+		target.remove();
+		console.log('removed');
+		return;
+	}
+
+	// create and style the overlay
+	let overlay = document.createElement('div');
+	overlay.style.height = target.height + 'px';
+	overlay.style.width = target.width + 'px';
+	overlay.style.backgroundColor = 'red';
+	overlay.style.position = 'absolute';
+	overlay.style.opacity = '0.3';
+
+
+
+	overlay.style.top = Number.parseInt(window.getComputedStyle(target).marginTop) + Math.abs(Number.parseInt(window.getComputedStyle(target.closest('.skills')).top)) + 'px';
+	overlay.style.left = target.offsetLeft + 'px';
+	overlay.classList.add('overlay');
+	var tmp = document.createElement("div");
+	tmp.appendChild(overlay);
+	target.insertAdjacentHTML('beforebegin', tmp.innerHTML);
+	// target.classList.add('active');
+});
